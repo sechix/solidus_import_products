8be3f76864c46163e07e149f47ce993e8917f754
sechix <sechix@gmail.com>
2017-11-30 19:17:49 +0100

    error in production

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -15,9 +15,20 @@ module Spree
 
     ENCODINGS= %w(UTF-8 iso-8859-1)
 
-    has_attached_file :data_file,
-                      :url => '/spree/product_data/data-files/:basename_:timestamp.:extension',
-                      :path => ":rails_root/public/spree/product_data/data-files/:basename_:timestamp.:extension"
+
+
+
+    if Rails.env.production?
+      has_attached_file :data_file,
+                        :url => '/import_products/product_data/data-files/:basename_:timestamp.:extension',
+                        :path => "#{ENV.fetch('CDN_HOST_SCHEME')}/import_products/product_data/data-files/:basename_:timestamp.:extension"
+    else
+      has_attached_file :data_file,
+                        :url => '/spree/product_data/data-files/:basename_:timestamp.:extension',
+                        :path => ":rails_root/public/spree/product_data/data-files/:basename_:timestamp.:extension"
+    end
+
+
     validates_attachment_presence :data_file
     #Content type of csv vary in different browsers.
     validates_attachment :data_file, :presence => true, content_type: {content_type: ["text/csv", "text/plain", "text/comma-separated-values", "application/octet-stream", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]}
