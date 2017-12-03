c4decad8ae8ba4f102052d91032823eebf8edbac
sechix <sechix@gmail.com>
2017-12-03 22:07:43 +0100

    error

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -107,7 +107,8 @@ module Spree
         log("import data start", :debug)
         @products_before_import = Spree::Product.all
         @skus_of_products_before_import = @products_before_import.map(&:sku)
-        @file = 'https://s3.eu-west-2.amazonaws.com/pislow' + self.data_file.path
+        @file = self.data_file.url
+        raise "#{@file}"
 
         rows = open_spreadsheet(@file)
 
