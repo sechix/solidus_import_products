e9e441ef060470333acda45e368dad9a38d264ad
sechix <sechix@gmail.com>
2017-12-03 18:48:30 +0100

    error

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -15,10 +15,7 @@ module Spree
 
     ENCODINGS= %w(UTF-8 iso-8859-1)
 
-      has_attached_file :data_file,
-                        :url => '/spree/product_data/data-files/:basename_:timestamp.:extension',
-                        :path => ":rails_root/public/spree/product_data/data-files/:basename_:timestamp.:extension"
-
+      has_attached_file :data_file
 
     validates_attachment_presence :data_file
     #Content type of csv vary in different browsers.
